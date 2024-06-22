d923ad7ab3a71d2edfbe7dedf1cf08af707a10b9
dev-ahad-ali <ahadali.dev@gmail.com>
2024-06-23 00:17:13 +0600

    server deployed

--- index.js
+++ index.js
@@ -415,8 +415,8 @@ async function run() {
       res.send(result);
     });
 
-    await client.db('admin').command({ ping: 1 });
-    console.log('Pinged your deployment. You successfully connected to MongoDB!');
+    // await client.db('admin').command({ ping: 1 });
+    // console.log('Pinged your deployment. You successfully connected to MongoDB!');
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
